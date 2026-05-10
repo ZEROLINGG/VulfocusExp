306636c3d057ebff2affc4c175c4c2173bb639d6
zz <nono@>
2026-05-10 21:33:38 +0800

    2026-05-10 21:33:38 更新

--- xxx.py
+++ xxx.py
@@ -3,7 +3,7 @@ from tool.base import parse_ip_port
 
 
 if __name__ ==  "__main__":
-    ip_port = "node5.buuoj.cn:26115"
+    ip_port = "192.168.192.148:39788,8022"
     tg = parse_ip_port(ip_port)
     for p,s in tg.detect_services():
         print(f"[{p}] {s}")
